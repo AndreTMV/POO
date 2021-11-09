963f210edae4549e8005664b6da2597742e3afee
AndreTMV <a19300104@ceti.mx>
2021-11-09 11:39:22 -0600

    Another stupid commit

--- 2do Parcial/constructores1.cpp
+++ 2do Parcial/constructores1.cpp
@@ -5,16 +5,21 @@ using std::cout, std::cin, std::endl;
 float sueldo;
 char puesto[30], nombre[40];
 
-void leer() {
+void leer()
+{
+  cout << "Sueldo: ";
   cin >> sueldo;
+  cout << "Puesto: ";
   cin.get();
   cin.get(puesto, 30);
+  cout << "Nombre: ";
   cin.get();
   cin.get(nombre, 40);
 }
 // constructor -> inicializa los objetos,lleva el mismo nombre de la clase
 // Cuidar que no exista ambigüedad
-class empleado {
+class empleado
+{
 public:
   float sueldo;
   char puesto[30];
@@ -23,19 +28,22 @@ public:
   empleado(float, char *, char *);
   void muestra();
 };
-empleado::empleado(float sueldo, char *puesto, char *nombre) {
+empleado::empleado(float sueldo, char *puesto, char *nombre)
+{
   empleado::sueldo = sueldo;
   strcpy(empleado::puesto, puesto);
   strcpy(empleado::nombre, nombre);
 }
 
-void empleado::muestra() {
-  cout << sueldo << endl;
-  cout << nombre << endl;
-  cout << puesto << endl;
+void empleado::muestra()
+{
+  cout << "Sueldo: " << sueldo << endl;
+  cout << "Nombre: " << nombre << endl;
+  cout << "Puesto: " << puesto << endl;
 }
 
-int main() {
+int main()
+{
   empleado ob1(0, " ", " ");
   empleado obj1(12000, "Programador", "André Mejía");
   cout << "Objeto 1:" << endl;
